7a2b59ed25d2b8fa77b531d4653773784f21fbb6
pcast31 <paul.casteras@student-cs.fr>
2024-03-22 14:24:28 +0100

    GRM debug + expérience taxi

--- ADVI.py
+++ ADVI.py
@@ -4,7 +4,8 @@ from autograd import elementwise_grad, jacobian
 
 
 class ADVI():
-    def __init__(self,inv_T,model,X):
+    def __init__(self,inv_T,model,X, dependant=True):
+        self.dependant = dependant
         self.inv_T = inv_T
         self.model = model
         self.X = X
@@ -14,7 +15,7 @@ class ADVI():
 
         self.log_jac_inv_T = self.log_jac
         self.grad_logjac_inv_T = elementwise_grad(self.log_jac_inv_T)
-
+        self.theta_size = model.theta_size
     def log_jac(self,z):
         return np.log(np.abs(np.linalg.det(jacobian(self.inv_T)(z))))
 
@@ -22,8 +23,11 @@ class ADVI():
         return self.model.log_distr(self.X,theta)
 
     def nu_to_zeta(self,nu_d):
-        return self.mu[-1]+self.omega[-1]@nu_d
-
+        if self.dependant:
+            return self.mu[-1]+self.omega[-1]@nu_d
+        else:
+            return self.mu[-1]+self.omega[-1]*nu_d
+            
     def zeta_to_theta(self,zeta):
         return self.inv_T(zeta)
 
@@ -33,15 +37,21 @@ class ADVI():
         for nu_i in nu:
             zeta_i = self.nu_to_zeta(nu_i)
             theta_i = self.zeta_to_theta(zeta_i)
-
             log_dis = self.grad_log_distribution(theta_i)
             grad_inv = self.grad_inv_T(zeta_i)
             grad_jac = self.grad_logjac_inv_T(zeta_i)
 
             self.grad_mu = self.grad_mu + log_dis*grad_inv+grad_jac
-            self.grad_omega = self.grad_omega+np.outer((log_dis*grad_inv+grad_jac),nu_i)
 
-        return self.grad_mu/len(nu), self.grad_omega/len(nu)+np.linalg.inv(self.omega[-1]).T
+            if self.dependant:
+                self.grad_omega = self.grad_omega+np.outer((log_dis*grad_inv+grad_jac),nu_i)
+            else:
+                self.grad_omega = self.grad_omega+log_dis*grad_inv+grad_jac*nu_i*np.exp(self.omega[-1])
+        if self.dependant:
+            meangrad_omega = self.grad_omega/len(nu) + np.linalg.inv(self.omega[-1]).T
+        else: 
+            meangrad_omega = self.grad_omega/len(nu) + 1
+        return self.grad_mu/len(nu), meangrad_omega
 
 
 
@@ -58,15 +68,21 @@ class ADVI():
         return lr * np.ones(P)
 
     def fit(self,eps=1e-4,max_step=1000,M=10, lr=1e-2, tau=1):
-        P = len(self.X[0])
+        P = self.theta_size
         N = len(self.X)
         iteration=1
         self.mu = [np.zeros(P)]
-        self.omega = [np.eye(P)]
+        if self.dependant:
+            self.omega = [np.eye(P)]
+        else: 
+            self.omega = [np.ones(P)]
         # TO DO :  Code to find optimal step size
         while iteration==1 or (np.linalg.norm(self.mu[-1]-self.mu[-2])+np.linalg.norm(self.omega[-1]-self.omega[-2])>eps and iteration<max_step):
-            nu = np.random.multivariate_normal(np.zeros(P),np.eye(P),M)
-
+            if self.dependant:
+                nu = np.random.multivariate_normal(np.zeros(P),np.eye(P),M)
+            else : 
+                nu = np.random.normal(0,1,M*P)
+                nu.reshape((P,M))
             gradient_mu,gradient_omega = self.compute_gradient_muomega(nu)
           #  gradient_omega = self.compute_gradient_omega(nu)
             step_vect = self.compute_step_size(iteration,P,lr)*iteration**-0.5
@@ -78,7 +94,7 @@ class ADVI():
             # self.omega.append(new_omega)
             self.omega.append(self.omega[-1]+step_vect*gradient_omega)
             iteration+=1
-            print(iteration,self.mu[-1],self.omega[-1]@self.omega[-1].T)
+            print(iteration,self.mu[-1],self.omega[-1]**2)
             print(np.linalg.norm(self.mu[-1]-self.mu[-2])+np.linalg.norm(self.omega[-1]-self.omega[-2]))
             
         return self.mu[-1],self.omega[-1]
